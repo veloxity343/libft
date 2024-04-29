0ff21e7e42ff68e7146dd6a418842ca9158c8087
Ryan Cheong <156203160+veloxity343@users.noreply.github.com>
2024-04-29 20:34:03 +0800

    Update ft_calloc.c

--- src/ft_mem/ft_calloc.c
+++ src/ft_mem/ft_calloc.c
@@ -14,19 +14,14 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*dest;
-	size_t	total_size;
+	void	*ptr;
 
 	if ((size != 0 && count > UINT_MAX / size)
 		|| (count != 0 && size > UINT_MAX / count))
-		dest = NULL;
-	else
-	{
-		total_size = count * size;
-		dest = malloc(total_size);
-		if (!dest)
-			return (0);
-		dest = ft_memset(dest, 0, total_size);
-	}
-	return (dest);
+		ptr = NULL;
+	ptr = (void *)malloc(size * count);
+	if (!ptr)
+		return (0);
+	ft_bzero(ptr, count * size);
+	return (ptr);
 }
